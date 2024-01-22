daf5e4cd918eea10a356b6a66a7cc28402b47c06
MamedYahyayev <yahyayev2001@gmail.com>
2024-01-22 23:46:38 +0400

    Init project

---
+++ src/test/java/az/aistgroup/MovieTheaterApplicationTests.java
@@ -0,0 +1,13 @@
+package az.aistgroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTheaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
