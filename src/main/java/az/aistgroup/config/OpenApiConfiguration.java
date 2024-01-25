6131978da52d4f1600629c63954a312cf956f47f
MamedYahyayev <yahyayev2001@gmail.com>
2024-01-25 17:59:10 +0400

    Add title, version, description to Open API configurations

--- src/main/java/az/aistgroup/config/OpenApiConfiguration.java
+++ src/main/java/az/aistgroup/config/OpenApiConfiguration.java
@@ -2,6 +2,7 @@ package az.aistgroup.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,11 @@ public class OpenApiConfiguration {
     public OpenAPI configureOpenApiSecurity() {
         final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
+                .info(new Info()
+                        .title("Movie Theater REST API")
+                        .version("V1")
+                        .description("Movie Theater REST API documentation.")
+                )
                 .addSecurityItem(new SecurityRequirement()
                         .addList(securitySchemeName))
                 .components(new Components()
